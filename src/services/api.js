4ba78ae1475b7cabac2300cf01c45a6d1db5704c
Pankajkumar12345678 <pankajkumar48132@gmail.com>
2025-10-31 09:55:59 +0530

    api

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-//const API_BASE_URL = 'https://aaklan-erp-backend.onrender.com/api';
+const API_BASE_URL = 'https://aaklan-erp-backend.onrender.com/api';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
